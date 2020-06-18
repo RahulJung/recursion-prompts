fafa2209a964ab3164fa9349646b9767644e5a99
RahulJung <rahuljungchauhan@gmail.com>
2020-06-18 16:33:19 -0400

    - Define palindrome() and make all the testcases pass

--- src/recursion.js
+++ src/recursion.js
@@ -128,6 +128,14 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  let newString = string.toLowerCase();
+  if(newString.length === 0 || newString.length === 1){
+    return true;
+  }else if(newString[0] === newString[newString.length - 1]){
+    return palindrome(newString.slice(1, newString.length - 1))
+  } else{
+    return false;
+  }
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
